b2962d48281a3f7a4c26f55b312ec5a68c3b2d7d
Dainer Mesa <dainer88@outlook.com>
2020-06-26 12:31:08 -0400

    update declare variable monitor enable

--- install_openvpn.sh
+++ install_openvpn.sh
@@ -432,7 +432,7 @@ function main() {
   declare -i FLAGS_API_PORT=1194
   declare -i FLAGS_KEYS_PORT=0
   declare -i FLAGS_MANAGEMENT_PORT=5555
-  declare -i FLAGS_MONITOR_ENABLE=false
+  declare FLAGS_MONITOR_ENABLE=false
   parse_flags "$@"
   install_openvpn
   if [[ FLAGS_MONITOR_ENABLE ]]; then
