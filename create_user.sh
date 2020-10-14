7dda5658bbd6a4b8bbe82a1e2a893d3ad569e723
Reisbel Machado <reisbel@gmail.com>
2020-10-14 11:58:23 -0400

    update add user file

--- create_user.sh
+++ create_user.sh
@@ -1,5 +1,5 @@
 function main() {
-  readonly SB_IMAGE=${SB_IMAGE:-kylemanna/openvpn}
+  readonly SB_IMAGE=${SB_IMAGE:-kylemanna/openvpn:2.3}
   export OPEN_VPN_DATA_DIR="${OPEN_VPN_DATA_DIR:-/opt/openvpn/vpn-data}"
 
   sudo docker run -v ${OPEN_VPN_DATA_DIR}:/etc/openvpn --rm -it ${SB_IMAGE} easyrsa build-client-full $1 nopass
